bda6e73430b6c9b0b26f986cf9e895eb2e38d4a7
Stephen Wilson <wilsons@start.ca>
2010-12-17 00:31:34 -0500

    Use ArchSpec's generic CPU types.

--- source/Plugins/Process/Utility/UnwindAssemblyProfiler-x86.cpp
+++ source/Plugins/Process/Utility/UnwindAssemblyProfiler-x86.cpp
@@ -844,11 +844,11 @@ UnwindAssemblyProfiler_x86::FirstNonPrologueInsn (AddressRange& func, Target& ta
 lldb_private::UnwindAssemblyProfiler *
 UnwindAssemblyProfiler_x86::CreateInstance (const lldb_private::ArchSpec &arch)
 {
-   uint32_t cpu = arch.GetCPUType ();
-   if (cpu != CPU_TYPE_X86_64 && cpu != CPU_TYPE_I386)
+    ArchSpec::CPU cpu = arch.GetGenericCPUType ();
+    if (cpu != ArchSpec::eCPU_x86_64 && cpu != ArchSpec::eCPU_i386)
        return NULL;
 
-   return new UnwindAssemblyProfiler_x86 (cpu == CPU_TYPE_X86_64 ? k_x86_64 : k_i386);
+   return new UnwindAssemblyProfiler_x86 (cpu == ArchSpec::eCPU_x86_64 ? k_x86_64 : k_i386);
 }
 
 
